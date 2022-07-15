a525cf9bad91d0ff5a36c4ee00a23e88049c41a0
ej8899 <ej8899@gmail.com>
2022-07-14 22:56:51 -0600

    initial upload - parse data, convert & error check

---
+++ timer1.js
@@ -0,0 +1,32 @@
+//
+// LHL - timer1.js project
+// expected usage: node timer1.js 10 3 5 15 9 
+// where beep occurs at the specified SECONDS
+// https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w4/activities/390?journey_step=32&workbook=7
+//
+
+//
+// COLLECT INPUT & convert to Int.
+//
+let inputData = process.argv.slice(2);
+
+//
+// INPUT CHECKS & CONVERSION
+// timer data as number array in parsedData[]
+//
+let parsedData= [];
+inputData.forEach(function (timeVal) {
+  if(Number(timeVal) > 0) { // filter out NaN and negatives
+    parsedData.push(Number(timeVal)); 
+  }
+} );
+// no data supplied (or remaining after error checks)
+if (parsedData.length < 1) {
+  console.log('No input for timers specified!\n\nUSAGE:  timer1.js x y z\nWhere x, y, z... etc can be any number of timers and seconds.\nExample: timer1.js 5 10 3');
+}
+
+//DEBUG error checking and input routines:
+console.log(parsedData);
+
+// START TIMERS
+
